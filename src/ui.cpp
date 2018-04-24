9f9402d215dd120e5ee19ad6da1aa315e4e8bb03
Matt Reyer <matthew5384@gmail.com>
2018-04-24 10:21:03 -0400

    Fixed backspace bug by updating ImGui keymap.

--- src/ui.cpp
+++ src/ui.cpp
@@ -82,8 +82,8 @@ void lb_ui_charCallback(unsigned int codePoint) {
 
 void lb_ui_keyCallback(int key, int scancode, int action, int mods) {
 	ImGuiIO& io = ImGui::GetIO();
-	if (action == 1) io.KeysDown[key] = true;
-	if (action == 0) io.KeysDown[key] = false;
+	if (action == GLFW_PRESS) io.KeysDown[key] = true;
+	if (action == GLFW_RELEASE) io.KeysDown[key] = false;
 }
 
 
@@ -122,7 +122,7 @@ static void renderImGuiDrawLists(ImDrawData* data) {
 	}
 }
 
-void SetStyleColors() {
+static void SetStyleColors() {
 	ImGuiStyle& style = ImGui::GetStyle();
 	ImVec4* colors = style.Colors;
 	style.FramePadding = ImVec2(4.0f,2.0f);
@@ -176,6 +176,31 @@ void SetStyleColors() {
 	colors[ImGuiCol_DragDropTarget]         = ImVec4(0.16f, 0.16f, 0.17f, 0.95f);
 }
 
+static void SetKeymap() {
+	ImGuiIO& io = ImGui::GetIO();
+	io.KeyMap[ImGuiKey_Tab] = GLFW_KEY_TAB;
+    io.KeyMap[ImGuiKey_LeftArrow] = GLFW_KEY_LEFT;
+    io.KeyMap[ImGuiKey_RightArrow] = GLFW_KEY_RIGHT;
+    io.KeyMap[ImGuiKey_UpArrow] = GLFW_KEY_UP;
+    io.KeyMap[ImGuiKey_DownArrow] = GLFW_KEY_DOWN;
+    io.KeyMap[ImGuiKey_PageUp] = GLFW_KEY_PAGE_UP;
+    io.KeyMap[ImGuiKey_PageDown] = GLFW_KEY_PAGE_DOWN;
+    io.KeyMap[ImGuiKey_Home] = GLFW_KEY_HOME;
+    io.KeyMap[ImGuiKey_End] = GLFW_KEY_END;
+    io.KeyMap[ImGuiKey_Insert] = GLFW_KEY_INSERT;
+    io.KeyMap[ImGuiKey_Delete] = GLFW_KEY_DELETE;
+    io.KeyMap[ImGuiKey_Backspace] = GLFW_KEY_BACKSPACE;
+    io.KeyMap[ImGuiKey_Space] = GLFW_KEY_SPACE;
+    io.KeyMap[ImGuiKey_Enter] = GLFW_KEY_ENTER;
+    io.KeyMap[ImGuiKey_Escape] = GLFW_KEY_ESCAPE;
+    io.KeyMap[ImGuiKey_A] = GLFW_KEY_A;
+    io.KeyMap[ImGuiKey_C] = GLFW_KEY_C;
+    io.KeyMap[ImGuiKey_V] = GLFW_KEY_V;
+    io.KeyMap[ImGuiKey_X] = GLFW_KEY_X;
+    io.KeyMap[ImGuiKey_Y] = GLFW_KEY_Y;
+    io.KeyMap[ImGuiKey_Z] = GLFW_KEY_Z;
+}
+
 static struct {
 	bool showFileSettingsPanel = false;
 	
@@ -217,6 +242,7 @@ EXTERN_C void lb_ui_init(
 	io.Fonts->TexID = (void *)(intptr_t) fontTextureID;
 		
 	SetStyleColors();
+	SetKeymap();
 	
 	// Load custom spritesheet
 	int ui_sprite_width, ui_sprite_height, ui_sprite_channels;
